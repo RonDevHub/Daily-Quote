3d1a233ea4067175106eb8a42ca32daf388fcd0c
RonDevHub <no_reply@rondev.de>
2026-07-06 08:58:33 +0000

    src/ShareHelper.php hinzugefügt

---
+++ src/ShareHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+class ShareHelper
+{
+    /**
+     * Generiert die Teilen-Links für verschiedene Plattformen.
+     */
+    public static function getLinks(string $text, string $author, string $currentUrl): array
+    {
+        $fullQuote = '"' . $text . '"';
+        if (!empty($author)) {
+            $fullQuote .= ' – ' . $author;
+        }
+        
+        $shareText = $fullQuote . ' | ' . $currentUrl;
+        $encodedText = urlencode($shareText);
+        $encodedUrl = urlencode($currentUrl);
+
+        return [
+            'facebook' => 'https://www.facebook.com/sharer/sharer.php?u=' . $encodedUrl,
+            'twitter'  => 'https://twitter.com/intent/tweet?text=' . urlencode($fullQuote) . '&url=' . $encodedUrl,
+            'threads'  => 'https://www.threads.net/intent/post?text=' . $encodedText,
+            'mastodon' => 'https://mastodonshare.com/?text=' . $encodedText,
+            'bluesky'  => 'https://bsky.app/intent/compose?text=' . $encodedText,
+            'whatsapp' => 'https://api.whatsapp.com/send?text=' . $encodedText
+        ];
+    }
+}
