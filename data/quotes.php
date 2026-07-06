60f72377ce3c4c870b286844da0b83b4df2f4e80
RonDevHub <no_reply@rondev.de>
2026-07-06 08:53:12 +0000

    data/quotes.php hinzugefügt

---
+++ data/quotes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Datenquelle für die täglichen Sprüche.
+ * Kann beliebig erweitert werden. Jede Zeile repräsentiert einen Spruch.
+ */
+return [
+    1 => [
+        'text' => 'Die Definition von Wahnsinn ist, immer wieder das Gleiche zu tun und andere Ergebnisse zu erwarten.',
+        'author' => 'Albert Einstein'
+    ],
+    2 => [
+        'text' => 'Es gibt nur einen Weg, um großartige Arbeit zu leisten: Tue, was du liebst.',
+        'author' => 'Steve Jobs'
+    ],
+    3 => [
+        'text' => 'Der beste Weg, die Zukunft vorherzusagen, ist, sie selbst zu gestalten.',
+        'author' => 'Alan Kay'
+    ],
+    4 => [
+        'text' => 'Nur wer sein Ziel kennt, findet den Weg.',
+        'author' => 'Laozi'
+    ],
+    5 => [
+        'text' => 'Machen ist wie wollen, nur krasser.',
+        'author' => 'Unbekannt'
+    ]
+];
